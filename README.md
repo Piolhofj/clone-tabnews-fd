39460bcb0c2e4aa4cf4a6d8acb95fadf81575a19
Piolhofj <maiconribeiromv@yahoo.com.br>
2025-01-30 12:11:29 -0300

    Adicionando scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews-fd
+
 Curso do Felipe Deschamps - Tabs News
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Obrigado por ser a melhor Mãe do Mundo!</h1>
+function Home() {
+  return <h1>Obrigado por ser a melhor Mãe do Mundo!</h1>;
 }
 
 export default Home;
